995d72422d7e721ca1086575e97c288fbf299c88
Rasel Hossain <raselhus@gmail.com>
2026-09-02 21:32:21 +0600

    added an js and login page conection

---
+++ script.js
@@ -0,0 +1,14 @@
+document.getElementById("login-buttom").addEventListener("click", function (e) {
+    e.preventDefault()
+    const mobileNumber = 12345678910
+
+    document.getElementById("mobile-number").value
+    const pinNumbers = 1234
+    document.getElementById("pin-number").value
+
+    console.log(e)
+})
+
+
+
+
