2e79eaea936527bd3552b4a1600159b62b3f2cbe
Rasel Hossain <raselhus@gmail.com>
2026-09-02 22:03:28 +0600

    if condition matched so that  a new file will open

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
 document.getElementById("login-buttom").addEventListener("click", function (e) {
     e.preventDefault()
     const mobileNumber = 12345678910
+    const pinNumber = 1234
 
-    document.getElementById("mobile-number").value
-    const pinNumbers = 1234
-    document.getElementById("pin-number").value
+    const mobileNumbervalue = document.getElementById("mobile-number").value
+    const mobileNumberValueConverted = parseInt(mobileNumbervalue)
 
-    console.log(e)
+
+    const pinNumberValue = document.getElementById("pin-number").value
+    const pinNumberValueConverted = parseInt(pinNumberValue)
+
+    // console.log(e)
+    console.log(mobileNumberValueConverted, pinNumberValueConverted)
+    if (mobileNumberValueConverted === mobileNumber && pinNumberValueConverted === pinNumber) {
+        // console.log("all values matched")
+        // new page dakabe
+        window.location.href = "./Home.html"
+    }
+    else {
+        // console.log("invalid credentials")
+        alert("invalid credentials")
+    }
 })
 
 
